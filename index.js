aa62c579fee535da94d1350a4bd530f72d801bc6
MOHAMMAD AHMED <mohammad.ahmed@daimler.com>
2022-07-29 11:12:52 +0530

    Create Store, state and listen to changes

---
+++ index.js
@@ -0,0 +1,27 @@
+function createStore(){
+    /**
+     * The state 
+     */
+    let state;
+    /**
+     * Getting the state
+     */
+    const getState = () => state;
+    /**
+     * Listening the changes
+     */
+    let listeners = [];
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            // To unsubscribe the changes
+            listeners.filter((l) => l != listener);
+        }
+    }
+
+    return {
+        getState,
+        subscribe
+    }
+
+}
