ee7a57c4395c1d6ad396adc55bc641a82d72a829
MOHAMMAD AHMED <mohammad.ahmed@daimler.com>
2022-07-29 11:21:26 +0530

    Updating the state

--- index.js
+++ index.js
@@ -18,10 +18,39 @@ function createStore(){
             listeners.filter((l) => l != listener);
         }
     }
+    /**
+     * Updating the state
+     */
+    const dispatch = (action) => {
+        state = todos(state, action);
+        listeners.forEach((listener) => listener());
+    }
+
+    function todos(state=[], action) {
+        if(action.type === "ADD_TODOS"){
+            return state.concat([action.todo]);
+        }
+        return state;
+    }
 
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 
 }
+
+const store = createStore();
+store.subscribe(() => {
+    console.log("The new state is:", store.getState());
+});
+
+store.dispatch({
+    type: "ADD_TODOS",
+    todo: {
+        id: 1,
+        name: "Learn Redux",
+        complete: false
+    }
+});
